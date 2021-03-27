770b48fb06a4823f9983fd84ad2827cff779caf4
4rtemTrickster <korneev.arty@yandex.ru>
2021-03-27 18:13:45 +0300

    Add some textures

--- RGR/src/Tests/Textured Cube/TestTexturedCube.cpp
+++ RGR/src/Tests/Textured Cube/TestTexturedCube.cpp
@@ -84,7 +84,7 @@ Test::TestTexturedCube::TestTexturedCube(Window* InWnd)
 	this->shader.SetUniformMat4f("u_View", view);
 	this->shader.SetUniformMat4f("u_Model", model);
 
-	this->texture.Init("res/Textures/Texture2.png");
+	this->texture.Init("res/Textures/Rock Texture/Base_color.png");
 
 	this->texture.Bind();
 	this->shader.SetUniform1i("u_Texture", 0);
