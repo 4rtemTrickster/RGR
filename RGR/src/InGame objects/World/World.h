5c3f8a9f9cef8210fbe3c0d6099e449bd0997a32
4rtemTrickster <korneev.arty@yandex.ru>
2021-04-04 21:48:45 +0300

    Update World.h

--- RGR/src/InGame objects/World/World.h
+++ RGR/src/InGame objects/World/World.h
@@ -5,8 +5,8 @@
 class World
 {
 public:
-	static const GLuint World_Width = 4;
-	static const GLuint World_Length = 4;
+	static const GLuint World_Width = 16;
+	static const GLuint World_Length = 16;
 	static const GLuint World_Volume = World_Width * World_Length;
 
 	std::vector<Chunk*> Chunks;
