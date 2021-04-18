a57183d64757bf8cd4b95be51a3c693c666023b0
4rtemTrickster <korneev.arty@yandex.ru>
2021-04-18 11:54:27 +0300

    pf

--- RGR/src/InGame objects/World/World.cpp
+++ RGR/src/InGame objects/World/World.cpp
@@ -74,50 +74,36 @@ World::World()
     LOG_INFO("World generated in {0} seconds", glfwGetTime() - start_time);
 }
 
-void World::PB(Mesh* mesh, const GLuint XStart, const GLuint ZStart, const GLuint XBorder, const GLuint ZBorder)
-{
-    mesh = World::ProcessBlock(0, 0, XBorder, ZBorder);
-}
-
 std::vector<Mesh*> World::GenerateMeshes()
 {
     std::vector<Mesh*> ret;
-    ret.reserve(4);
-    Mesh* LB;
-    Mesh* RB;
-    Mesh* LT;
-    Mesh* RT;
+    ret.resize(4);
+ 
     
     auto lb = std::async(std::launch::async, [&]()
     {
-        LB = ProcessBlock();
+        ret[0] = ProcessBlock();
     });
 
     auto rb =std::async(std::launch::async, [&]()
     {
-        RB = ProcessBlock(World_Width/2,0, World_Width, World_Length/2);
+        ret[1] = ProcessBlock(World_Width/2,0, World_Width, World_Length/2);
     });
 
     auto lt = std::async(std::launch::async, [&]()
     {
-        LT =  ProcessBlock(0,World_Length/2, World_Width/2, World_Length);
+        ret[2] =  ProcessBlock(0,World_Length/2, World_Width/2, World_Length);
     });
 
     auto rt =std::async(std::launch::async, [&]()
     {
-        RT = ProcessBlock(World_Width/2, World_Length/2, World_Width, World_Length);
-
+        ret[3] = ProcessBlock(World_Width/2, World_Length/2, World_Width, World_Length);
     });
 
     lb.wait();
     rb.wait();
     lt.wait();
     rt.wait();
-
-    ret.push_back(LB);
-    ret.push_back(RB);
-    ret.push_back(LT);
-    ret.push_back(RT);
 
     
     return ret;
